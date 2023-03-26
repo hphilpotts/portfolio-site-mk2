317a754186b2babba545013ceebe8cc76412d4ae
Henry Philpotts <hphilpotts@gmail.com>
2023-03-26 17:07:04 +0100

    Welcome page updated.

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ const introScreen = document.getElementById('intro-screen')
 navLogoLink.addEventListener('click', function () { scrollToTargetElement(introScreen) })
 downArrow.addEventListener('click', function () { scrollToTargetElement(introScreen) })
 
-const projectsLink = document.getElementById('projects-link');
-const projectsScreen = document.getElementById('projects-screen');
+const projectsLink = document.getElementById('projects-link')
+const projectsSpan = document.getElementById('projects-span')
+const projectsScreen = document.getElementById('projects-screen')
 projectsLink.addEventListener('click', function () { scrollToTargetElement(projectsScreen) })
+projectsSpan.addEventListener('click', function () { scrollToTargetElement(projectsScreen) })
 
 const aboutLink = document.getElementById('about-link')
+const aboutSpan = document.getElementById('about-span')
 const aboutScreen = document.getElementById('about-screen')
 aboutLink.addEventListener('click', function () { scrollToTargetElement(aboutScreen) })
+aboutSpan.addEventListener('click', function () { scrollToTargetElement(aboutScreen) })
