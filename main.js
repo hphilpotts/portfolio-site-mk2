6a83a2d7b59b8a326f8314b1260524c41ff8dd4a
Henry Philpotts <hphilpotts@gmail.com>
2023-03-26 16:36:07 +0100

    Landing page upadated with down arrow.

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const scrollToTargetElement = targetElement => targetElement.scrollIntoView({ behavior: "smooth" });
 
+const navLogoLink = document.getElementById('navbar-logo')
+const downArrow = document.getElementById('down-arrow')
+const introScreen = document.getElementById('intro-screen')
+navLogoLink.addEventListener('click', function () { scrollToTargetElement(introScreen) })
+downArrow.addEventListener('click', function () { scrollToTargetElement(introScreen) })
+
 const projectsLink = document.getElementById('projects-link');
 const projectsScreen = document.getElementById('projects-screen');
 projectsLink.addEventListener('click', function () { scrollToTargetElement(projectsScreen) })
@@ -7,7 +13,3 @@ projectsLink.addEventListener('click', function () { scrollToTargetElement(proje
 const aboutLink = document.getElementById('about-link')
 const aboutScreen = document.getElementById('about-screen')
 aboutLink.addEventListener('click', function () { scrollToTargetElement(aboutScreen) })
-
-const navLogoLink = document.getElementById('navbar-logo')
-const introScreen = document.getElementById('intro-screen')
-navLogoLink.addEventListener('click', function () { scrollToTargetElement(introScreen) })
