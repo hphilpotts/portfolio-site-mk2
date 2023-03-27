00f4c8bb77f65296c473ddcd6e0a6b06387efaa7
Henry Philpotts <hphilpotts@gmail.com>
2023-03-27 17:14:57 +0100

    Projects slider logic implementation begun: slide in view is detected correctly.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Navigation
+
 const scrollToTargetElement = targetElement => targetElement.scrollIntoView({ behavior: "smooth" });
 
 const navLogoLink = document.getElementById('navbar-logo')
@@ -17,3 +19,41 @@ const aboutSpan = document.getElementById('about-span')
 const aboutScreen = document.getElementById('about-screen')
 aboutLink.addEventListener('click', function () { scrollToTargetElement(aboutScreen) })
 aboutSpan.addEventListener('click', function () { scrollToTargetElement(aboutScreen) })
+
+// Project Slider Behaviour
+
+const sliderIcons = Array.prototype.slice.call( document.getElementsByClassName('icon-img') )
+const sliderBCRLeft = document.getElementById('slider').getBoundingClientRect().left;
+const slides = Array.prototype.slice.call( document.getElementsByClassName('slide') )
+
+const checkAllSliderBCRLefts = () => {
+    for (let i = 0; i < 7; i++) {
+        if (checkSliderBCRLeft(i, sliderBCRLeft)) {
+            return i
+        }
+    }
+}
+
+const checkSliderBCRLeft = (index, checkpoint) => {
+    const proximityBetweenElements = (slides[`${index}`].getBoundingClientRect().left / checkpoint)
+    return (proximityBetweenElements > 0.9 && proximityBetweenElements < 1.1)
+}
+
+// const illuminateIconsAtIndeces = {
+//     0: [],
+//     1: [0, 1, 2, 4, 5],
+//     2: [],
+//     3: [],
+//     4: [],
+//     5: [],
+//     6: [],
+// }
+
+slider.addEventListener('scroll', function(){
+    const currentSlide = checkAllSliderBCRLefts()
+    if (currentSlide === undefined) {
+        sliderIcons.forEach(icon => icon.classList.add('hidden'))
+    } else {
+        sliderIcons[`${currentSlide}`].classList.add('hidden')
+    }
+})
