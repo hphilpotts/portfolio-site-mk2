c0b503db3ad9dcbd141f916dfdb7f750622abe9d
Henry Philpotts <hphilpotts@gmail.com>
2023-04-08 16:35:03 +0100

    main.js refactored.

--- main.js
+++ main.js
@@ -2,59 +2,59 @@
 
 const scrollToTargetElement = targetElement => targetElement.scrollIntoView({ behavior: "smooth" });
 
-const navLogoLink = document.getElementById('navbar-logo')
+class LinkGroup {
+    constructor(link) {
+        this.links = [document.getElementById(link + '-link'), document.getElementById(link + '-link-mobile'), document.getElementById(link + '-span')]
+        this.targetScreen = document.getElementById(link + '-screen')
+
+        const target = this.targetScreen
+        const filteredLinks = this.links.filter(Boolean)
+        filteredLinks.forEach(link => link.addEventListener('click', function () { scrollToTargetElement(target) }))
+    }
+}
+
+const projectsLinks = new LinkGroup('projects')
+const aboutLinks = new LinkGroup('about')
+const helloLinks = new LinkGroup('hello')
+
 const downArrow = document.getElementById('down-arrow')
-const introScreen = document.getElementById('intro-screen')
-navLogoLink.addEventListener('click', function () { scrollToTargetElement(introScreen) })
-downArrow.addEventListener('click', function () { scrollToTargetElement(introScreen) })
-
-const projectsLink = document.getElementById('projects-link')
-const projectsSpan = document.getElementById('projects-span')
-const projectsScreen = document.getElementById('projects-screen')
-projectsLink.addEventListener('click', function () { scrollToTargetElement(projectsScreen) })
-projectsSpan.addEventListener('click', function () { scrollToTargetElement(projectsScreen) })
-
-const aboutLink = document.getElementById('about-link')
-const aboutSpan = document.getElementById('about-span')
-const aboutScreen = document.getElementById('about-screen')
-aboutLink.addEventListener('click', function () { scrollToTargetElement(aboutScreen) })
-aboutSpan.addEventListener('click', function () { scrollToTargetElement(aboutScreen) })
+downArrow.addEventListener('click', function () { scrollToTargetElement(helloLinks.targetScreen) })
 
 // Project Slider Behaviour
 
 const sliderIcons = Array.prototype.slice.call(document.getElementsByClassName('icon-img'))
 const slides = Array.prototype.slice.call(document.getElementsByClassName('slide'))
 
-let sliderBCRLeft = 0
-const getSliderBCRLeft = () => sliderBCRLeft = document.getElementById('slider').getBoundingClientRect().left
-getSliderBCRLeft()
-window.onresize = getSliderBCRLeft
+let sliderLeft = 0
+const getSliderLeft = () => sliderLeft = document.getElementById('slider').getBoundingClientRect().left
+getSliderLeft()
+window.onresize = getSliderLeft
 
-const checkAllSliderBCRLefts = () => {
+const checkAllSliderLefts = () => {
     for (let i = 0; i < 7; i++) {
-        if (checkSliderBCRLeft(i, sliderBCRLeft)) {
+        if (checkSliderLeft(i, sliderLeft)) {
             return i
         }
     }
 }
 
-const checkSliderBCRLeft = (index, checkpoint) => {
+const checkSliderLeft = (index, checkpoint) => {
     const proximityBetweenElements = (slides[`${index}`].getBoundingClientRect().left / checkpoint)
     return (proximityBetweenElements > 0.9 && proximityBetweenElements < 1.1)
 }
 
-const illuminateIconsAtIndeces = {
-    0: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-    1: [0, 1, 2, 4, 5, 6, 7, 12, 13, 14],
-    2: [0, 1, 2, 3],
-    3: [0, 1, 2, 4, 5, 6, 7, 12, 13, 14],
-    4: [0, 1, 8, 9, 10, 12],
-    5: [0, 1, 2, 4, 5, 7, 11, 15],
-    6: [0, 1, 2, 15],
-}
+const illuminateIconsAtIndeces = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    [0, 1, 2, 4, 5, 6, 7, 12, 13, 14],
+    [0, 1, 2, 3],
+    [0, 1, 2, 4, 5, 6, 7, 12, 13, 14],
+    [0, 1, 8, 9, 10, 12],
+    [0, 1, 2, 4, 5, 7, 11, 15],
+    [0, 1, 2, 15],
+]
 
 slider.addEventListener('scroll', function () {
-    const currentSlide = checkAllSliderBCRLefts()
+    const currentSlide = checkAllSliderLefts()
     const currentIlluminations = illuminateIconsAtIndeces[currentSlide]
     if (currentSlide === undefined) {
         $(".icon-img").fadeOut(175)
@@ -67,11 +67,32 @@ slider.addEventListener('scroll', function () {
 
 const slideBody = {};
 
-for (let i = 0; i < 7; i++) {
-    slideBody[`${i}`] = {
+for (let i = 1; i < 7; i++) {
+    slideBody[`slideIndex${i}`] = {
         buttons: $('#slide-' + i + '-btn').children().toArray(),
         slides: $('#slide-' + i + '-body').children().toArray()
     }
+}
+
+for (let slideIndex in slideBody) {
+
+    const slideButtons = slideBody[slideIndex].buttons
+    const slideSlides = slideBody[slideIndex].slides
+
+    slideButtons.forEach(button => {
+
+        const targetSlide = '#item' + button.id.slice(-4)
+        button.addEventListener('click', function () {
+
+            showTargetedSlide(targetSlide, slideSlides)
+
+            slideButtons.forEach(eachButton => {
+                $(eachButton).removeClass('bold-highlighted')
+            })
+            $(button).addClass('bold-highlighted')
+        })
+
+    })
 }
 
 const showTargetedSlide = (targetSlide, slideArray) => {
@@ -79,19 +100,4 @@ const showTargetedSlide = (targetSlide, slideArray) => {
         $(slide).fadeOut(200)
     })
     $(targetSlide).fadeIn(400)
-}
-
-for (let prop in slideBody) {
-    const slideButtons = slideBody[prop].buttons
-    const slideSlides = slideBody[prop].slides
-    slideButtons.forEach(button => {
-        const targetSlide = '#item' + button.id.slice(-4)
-        button.addEventListener('click', function () {
-            showTargetedSlide(targetSlide, slideSlides)
-            slideButtons.forEach(eachButton => {
-                $(eachButton).removeClass('bold-highlighted')
-            })
-            $(button).addClass('bold-highlighted')
-        })
-    })
 }
